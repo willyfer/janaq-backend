b539eb3d60be0e88600e2336c375217faa447428
willy <willyferg10@gmail.com>
2021-02-10 20:54:01 -0500

    update email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -9,17 +9,26 @@ class EmailController extends Controller
     
     public function contact(Request $request){
 
-          $credentials = Request('email');
-         $emailsend = $request->email();
+          
+        $emailsend = $request->input('email');
         $subject = "Asunto del correo test";
-        $for = $credentials;
-        Mail::send('email',$request->all(), function($msj) use($subject,$for){
-            $msj->from("jhairferg@gmail.com","test_email");
-            $msj->subject($subject);
-            $msj->to($for);
-        });
+         
+        
+                
+                $config = [
+                    "email_client" =>$emailsend
+                ];
+            $correoEmpresa = env('MAIL_USERNAME');
+            Mail::send('Email', $config, function ($message) use ($correoEmpresa,$emailsend) {
+                    
+                $message->from($correoEmpresa);
+                $message->subject('Notificación - [WILLY]');
+                $message->to($emailsend);
+
+            });
+        
          return response()->json([
-            'message' => 'mesaje enviado a  registrado.'+ $emailsend,
+            'message' => 'mesaje enviado a  registrado.' . $emailsend,
              
         ], 200);
     }
